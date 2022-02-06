c45908ead98106392cc9b81db56b1f11680c624c
zoniha <awaawapurin@gmail.com>
2022-02-06 14:56:43 +0900

    ✨ feat: Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+import { useState } from 'react';
 import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
+  const [items] = useState(['item0', 'item1', 'item2']);
+  const onDragEnd = (result) => {
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
+
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
-            <div className="item" {...provided.droppableProps} ref={provided.innerRef}>
+            <div {...provided.droppableProps} ref={provided.innerRef}>
               <Draggable draggableId="item0" index={0}>
                 {(provided) => (
                   <div
+                    className="item"
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                   >
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
